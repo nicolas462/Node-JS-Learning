312cd40ea9921c227b223a76381fee7ca8cecc30
Nicolás David Espejo Bernal <nespejobern@uniminuto.edu.co>
2021-07-09 20:25:55 -0500

    working with socket.io

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
@@ -16,11 +18,15 @@ app.get('/messages', (req, res) =>{
 })
 
 app.post('/messages', (req, res) =>{
-    console.log(req.body)
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
 })
 
-var server = app.listen(3000, () =>{
+io.on('connection', (socket) => {
+    console.log('user connected')
+})
+
+var server = http.listen(3000, () =>{
     console.log('Server is listening on port ', server.address().port)
 })
