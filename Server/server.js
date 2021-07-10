b50a2a3c2dab7108be619f6bad1c27f3604afe3d
Nicolás David Espejo Bernal <nespejobern@uniminuto.edu.co>
2021-07-09 21:11:08 -0500

    working with mongoDB

--- Server/server.js
+++ Server/server.js
@@ -3,28 +3,44 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-    {name: 'Tim', message: 'I am Tim.'},
-    {name: 'Sam', message: 'I am Sam.'}
-]
+var dbUrl = 'mongodb+srv://root:6hIAiDdXJU8uQmae@cluster0.kzkr1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+//Entity
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req, res) =>{
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
 app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+
+    message.save((err) => {
+        if (err)
+            res.sendStatus(500)
+
+        io.emit('message', req.body)
+        res.sendStatus(200)  
+    })
 })
 
 io.on('connection', (socket) => {
     console.log('user connected')
+})
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true },(err) => {
+    console.log('mongoDB connection', err)
 })
 
 var server = http.listen(3000, () =>{
